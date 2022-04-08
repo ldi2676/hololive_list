8cd45b74cf0bee2919b08f2a6d7e970f9c7d8cee
leedongik <jw2676@naver.com>
2022-04-09 00:18:36 +0900

    Update hololive.ts

--- src/hololive.ts
+++ src/hololive.ts
@@ -43,11 +43,19 @@ app.get('/hololive_member_image', (req: Request, res: Response) => {
 	})
 });
 
+app.get('/hololive_list', (req: Request, res: Response) => {
+	const query: string = 'select * from hololive_list';
+	dbc.query(query, (err, rows) => {
+		if (err) return console.log(err);
+		res.send(rows);
+	})
+});
+
 app.get('/select', (req: Request, res: Response) => {
 	// NOTE: template literal 사용시 backticks ` (grave accents) 사용. (따옴표 아님 주의)
 	//       키보드 esc 아래, ~ (Shift 안 누르고 입력), 1 왼쪽
 	// 예제 https://express-gunpladb-wwjww.run.goorm.io/select?id=1
-	const query: string = `select * from hololive_company where id = ${req.query.id}`;
+	const query: string = `select * from hololive_list where id = ${req.query.id}`;
 	dbc.query(query, (err, rows) => {
 		if (err) return console.log(err);
 		res.send(rows);
@@ -55,7 +63,7 @@ app.get('/select', (req: Request, res: Response) => {
 });
 
 app.get('/select_id', (req: Request, res: Response) => {
-	const query: string = "select * from hololive_company where id = ?";
+	const query: string = "select * from hololive_list where id = ?";
 	dbc.query(query, [ req.query.id ], (err, rows) => {
 		if (err) return console.log(err);
 		res.send(rows);
