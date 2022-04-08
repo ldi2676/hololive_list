6469528b2754347bf4b476f4ade0a4ef07f6346c
leedongik <jw2676@naver.com>
2022-04-09 00:27:54 +0900

    Update hololive.ts

--- src/hololive.ts
+++ src/hololive.ts
@@ -19,6 +19,14 @@ app.listen('3030', () => {
 	console.log('Server Started')
 })
 
+app.get('/hololive_list', (req: Request, res: Response) => {
+	const query: string = 'select * from hololive_list';
+	dbc.query(query, (err, rows) => {
+		if (err) return console.log(err);
+		res.send(rows);
+	})
+});
+
 app.get('/hololive_company', (req: Request, res: Response) => {
 	const query: string = 'select * from hololive_company';
 	dbc.query(query, (err, rows) => {
@@ -37,14 +45,6 @@ app.get('/hololive_member', (req: Request, res: Response) => {
 
 app.get('/hololive_member_image', (req: Request, res: Response) => {
 	const query: string = 'select * from hololive_member_image';
-	dbc.query(query, (err, rows) => {
-		if (err) return console.log(err);
-		res.send(rows);
-	})
-});
-
-app.get('/hololive_list', (req: Request, res: Response) => {
-	const query: string = 'select * from hololive_list';
 	dbc.query(query, (err, rows) => {
 		if (err) return console.log(err);
 		res.send(rows);
